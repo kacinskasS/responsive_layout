45dde03b9c51a9b042c47827d6240a09072ae725
kacinskasS <53061402+kacinskasS@users.noreply.github.com>
2019-12-10 09:22:42 +0200

    Add files via upload

--- 100%_mokytojas/script.js
+++ 100%_mokytojas/script.js
@@ -1,12 +1,31 @@
-var menu_toggle = document.getElementById('menu_toggle');
+var menu_toggle = document.getElementById('menu-toggle');
 
 menu_toggle.addEventListener('click', toggle_menu);
 
 function toggle_menu() {
-    var navList = document.getElementById("show_menu");
+    var navList = document.getElementById("overlay-menu");
     if (navList.classList.contains('show')) {
-        navList.classList.remove('show')
+        navList.classList.remove('show');
+        document.body.classList.remove('no-scroll');
     } else {
-        navList.classList.add('show')
+        navList.classList.add('show');
+        document.body.classList.add('no-scroll');
     }
 }
+
+function load() {
+    var macy = Macy({
+        container: '#nominees',
+        trueOrder: false,
+        waitForImages: false,
+        margin: 15,
+        columns: 3,
+        breakAt: {
+            1142: 2,
+            768: 1
+        }
+    });
+
+}
+
+window.addEventListener('load', load);
